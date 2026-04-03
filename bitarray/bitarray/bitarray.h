01188969d373528f1e48369c97c020e26395e5ed
Lemuren <h.e.thorsell@gmail.com>
2026-04-03 17:51:11 +0000

    Add docstrings

--- bitarray/bitarray/bitarray.h
+++ bitarray/bitarray/bitarray.h
@@ -1,3 +1,9 @@
+//! bitarray: A dynamic array of bits.
+//!
+//! Provides simple operations for setting, clearing,
+//! and reading individual bits. Bits are stored LSB-first
+//! within each byte.
+
 #ifndef TBC_BITARRAY_H
 #define TBC_BITARRAY_H
 
@@ -6,23 +12,36 @@
 
 typedef struct _bitarray * bitarray;
 
-// Create a new bitarray of count bits.
+/// Create a new bitarray with `count` bits.
+/// @param count Number of bits.
+/// @return A new bitarray.
 bitarray bitarray_init(size_t count);
 
-// Free a bitarray.
+/// Free a bitarray.
+/// @param b The bitarray to free.
 void bitarray_destroy(bitarray b);
 
-// Set bit n. Returns the value of the bit that was set.
+/// Set bit n.
+/// @param b The bitarray.
+/// @param n The bit index.
+/// @return The previous value of the bit.
 bool set(bitarray b, size_t n);
 
-// Clear bit n. Returns the value of the bit that was cleared.
+/// Clear bit n.
+/// @param b The bitarray.
+/// @param n The bit index.
+/// @return The previous value of the bit.
 bool clear(bitarray b, size_t n);
 
-// Read bit n.
+/// Read bit n.
+/// @param b The bitarray.
+/// @param n The bit index.
+/// @return True if the bit is set.
 bool get(bitarray b, size_t n);
 
-// Returns the length (count) of a bitarray.
+/// Returns the length (count) of a bitarray.
+/// @param b The bitarray.
+/// @return Number of bits.
 size_t bitarray_len(bitarray b);
 
 #endif // TBC_BITARRAY_H
-
